66a38fd5650b696a530db1f9a78f3721c12d95f7
Luzerz <amonchai@wedev.co.th>
2020-05-19 11:48:20 +0700

    Authentication

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/luzerz/Authentication
+
+go 1.14
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/myesui/uuid v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/twinj/uuid v1.0.0
+	gopkg.in/stretchr/testify.v1 v1.2.2 // indirect
+)
---
+++ redisdb.go
@@ -0,0 +1,20 @@
+package Authentication
+
+import "github.com/go-redis/redis/v7"
+
+type RedisService struct {
+	Auth   Interface
+	Client *redis.Client
+}
+
+func NewRedisDB(host, port, password string) (*RedisService, error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     host + ":" + port,
+		Password: password,
+		DB:       0,
+	})
+	return &RedisService{
+		Auth:   NewAuth(redisClient),
+		Client: redisClient,
+	}, nil
+}
